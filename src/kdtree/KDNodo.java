7ecbd7ff80f8605a5d7974a72e4cdff727d087d2
x <iefh00@gmail.com>
2022-05-16 16:20:47 -0400

    4

--- src/kdtree/KDNodo.java
+++ src/kdtree/KDNodo.java
@@ -1,12 +1,14 @@
 package kdtree;
 
 public class KDNodo {
+    int factor;
     int X;
     int Y;
     KDNodo right;
     KDNodo left;
 
-    public KDNodo(int x,int y){
+    public KDNodo(int x,int y,int f){
+        this.factor = f;
         this.X = x;
         this.Y= y;
         right = null;
--- src/kdtree/KDTree.java
+++ src/kdtree/KDTree.java
@@ -3,12 +3,9 @@ package kdtree;
 public class KDTree {
 
     public KDNodo root;
-    public int height;
 
     public KDTree(){
         root = null;
-        height = 0;
-
     }
 
 
