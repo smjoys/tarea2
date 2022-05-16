5f934ce7f13920e24b0e960e2e07966988589c09
x <iefh00@gmail.com>
2022-05-16 14:45:46 -0400

    2

--- src/kdtree/KDTree.java
+++ src/kdtree/KDTree.java
@@ -1,4 +1,22 @@
 package kdtree;
 
 public class KDTree {
+    private int X;
+    private int Y;
+    private KDTree right;
+    private KDTree left;
+    public KDTree(int x,int y,KDTree r,KDTree l){
+        this.X = x;
+        this.Y= y;
+        right = r;
+        left=l;
+    }
+    private KDTree root;
+    private int size;
+
+    public KDTree() {
+        root = null;
+        size = 0;
+    }
+
 }
--- src/vista/Main.java
+++ src/vista/Main.java
@@ -1,4 +1,6 @@
 package vista;
 
 public class Main {
+
+
 }
