49615ef273760db4a759929e302ee3d175691210
x <iefh00@gmail.com>
2022-05-17 21:06:06 -0400

    5

--- src/kdtree/KDTree.java
+++ src/kdtree/KDTree.java
@@ -8,5 +8,34 @@ public class KDTree {
         root = null;
     }
 
+    public void insertar(KDNodo n){
+        if (root == null) root=n;
+        else revEdad(n,root);
+    }
+
+    private void revEdad(KDNodo neu,KDNodo act){
+        if (neu.edad < act.edad) {
+            if (act.left == null) {
+                act.left=neu;
+            }else revPeso(neu,act.left);
+        }else {
+            if (act.right == null) {
+                act.right=neu;
+            }else revPeso(neu,act.right);
+        }
+    }
+    private void revPeso(KDNodo neu,KDNodo act){
+        if (neu.peso < act.peso) {
+            if (act.left == null) {
+                act.left = neu;
+            }else revEdad(neu,act.left);
+        }else {
+            if (act.right == null) {
+                act.right = neu;
+            }else revEdad(neu,act.right);
+        }
+    }
+
+
 
 }
